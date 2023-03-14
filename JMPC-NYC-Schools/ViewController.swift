ff23aca13ec75d7781a4abdd175991fb93d7d51c
ramirourista <ramiro.urista@hotmail.com>
2023-03-14 12:37:40 -0700

    Initial Commit

---
+++ JMPC-NYC-Schools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JMPC-NYC-Schools
+//
+//  Created by Ramiro on 3/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ JMPC-NYC-SchoolsTests/JMPC_NYC_SchoolsTests.swift
@@ -0,0 +1,33 @@
+//
+//  JMPC_NYC_SchoolsTests.swift
+//  JMPC-NYC-SchoolsTests
+//
+//  Created by Ramiro on 3/14/23.
+//
+
+import XCTest
+@testable import JMPC_NYC_Schools
+
+class JMPC_NYC_SchoolsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
